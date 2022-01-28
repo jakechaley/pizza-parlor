d490cb355c49a227e2e335563c5a264dc90b7637
jakechaley <jakechaley@gmail.com>
2022-01-28 10:32:51 -0800

    make test 2 work

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 // TESTS
 
 // Describe Pizza()
-// Test: "It should return a pizza oject with two properties for toppings and size"
+// Test: "It should return a pizza object with two properties for toppings and size"
 // code: const myPizza = new Pizza ('pepperoni', 'medium');
 // Expected Output: Pizza { toppings: ['pepperoni', 'sausage'], size: 'medium' }
+
+// Describe Pizza.prototype.totalCost ()
+// Test: It should add up the cost based on size and topping choice.
+// code: const myPizza = new Pizza ('pepperoni', 'medium',);
+// Expected Output: Pizza { toppings: 'pepperoni', size: 'medium', cost: 12 }
--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,40 @@ function Pizza(topping, size) {
   this.topping = topping;
   this.size = size;
   this.cost = 0;
-}
+};
+
+Pizza.prototype.totalCost = function(myPizza) {
+  if (this.size === 'small') {
+    this.cost += 8;
+  } else if (this.size === 'medium') {
+    this.cost += 10;
+  } else if (this.size === 'large') {
+    this.cost += 13;
+  } else {
+    this.cost += 15;
+  };
+
+  if (this.topping === 'pepperoni') {
+    this.cost += 2;
+  } else if (this.topping === 'italianSausage') {
+    this.cost += 3;
+  } else if (this.topping === 'greenPepper') {
+    this.cost += 4;
+  } else if (this.topping === 'pineapple') {
+    this.cost += 1;
+  } else {
+    this.cost += 0;
+  };
+  console.log(this.cost);
+};
+
+$(document).ready(function() {
+  $('#userInput').submit(function(event) {
+    event.preventDefault();
+    let topping = $('#pizzaTopping').val();
+    let size = $('#pizzaSize').val();
+    let myPizza = new Pizza(topping, size);
+    myPizza.totalCost();
+
+  });
+});
